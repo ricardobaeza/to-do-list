bb5ac238abacf7b080abfa99decb6ea7fb3e5d73
Ricardo Baeza <baezaric3600@gmail.com>
2018-10-15 18:59:57 -0600

    added editing

--- custom.js
+++ custom.js
@@ -1,7 +1,11 @@
 
 
 
-
+$('#myInput').on('keydown', function (event) {
+    if (event.which === 13) {
+        newElement();
+    }
+});
 
 
 let newElement = () => {
@@ -9,7 +13,7 @@ let myinput = $('#myInput').val();
     if (myinput === '') {
         alert('you have to enter something in the text field');
     } else {
-        $('#list').append('<div class="listItemContainer">' + '<div contenteditable="true" class="listItem">' + myinput +
+        $('#list').append('<div class="listItemContainer">' + '<div contenteditable="true" class="card-body listItem">' + myinput +
             '</div>'  + '<span>' + '<i class=" delete far fa-times-circle"></i>' + '<i class="completed fas fa-check"></i>' + '</span>' + '</div>');
     }
     $('#myInput').val("");
@@ -19,15 +23,12 @@ let myinput = $('#myInput').val();
 
     $('.completed').click(function () {
         $(this).parent().parent().css('background-color', '#62f774');
-        myinput.style.textDecoration = 'line-through';
+        $(this).parent().parent().addClass('deleteComplete');
     })
 };
 
 
 
-
-
-
-let  completedListItem = () => {
-    console.log('you got your check mark to work now make something with it')
+let deleteCompleted = () => {
+    $('.deleteComplete').remove();
 }
